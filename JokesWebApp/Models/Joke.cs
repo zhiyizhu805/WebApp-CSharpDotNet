d9dfe14b485068f2d7eb6dd0dc3798eb119310d0
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-02-03 11:07:42 +1300

    add JokesController

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace JokesWebApp.Models
+{
+	public class Joke
+	{
+		    // prop shortcut to generate prop
+			public int Id { get; set; }
+		    public string JokeQuestion { get; set; }
+		    public string JokeAnswer { get; set; }
+
+		// ctor shortcut to generate method
+		public Joke()
+		{
+
+		}
+
+	}
+}
+
